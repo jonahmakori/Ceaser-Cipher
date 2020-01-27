6f2bc7ab873e7a4759dbf4de9ce65ecee4922f2d
jonahmakori <jonahmakori0@gmail.com>
2020-01-27 17:46:01 +0300

    debugged in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
 import java.util.*;
-
+import java.util.Scanner;
 public class App {
     public static void main(String args[]) {
      Scanner scl = new Scanner(System.in);
+       boolean programRunning = true;
+       while(programRunning) {
              System.out.println("Welcome to Caeser Cipher\n" +
                      "------------------------------------------------\n" +
                      "Choose:\n 1 for Encryption\n 2 for Decryption\n # to Exit");
@@ -18,7 +20,7 @@ public class App {
                  Cipher theTest = new Cipher(mText,mNumbers);
                  System.out.println("Your encrypted text is:" + theTest.EncryptedText());
                  System.out.println("-----------------------------------------------");
-             } else if (select.equals("2")) {
+              } else if (select.equals("2")) {
                  System.out.println("-----------------------------------------------");
                  System.out.println("Input the cipher text message tobe decrypted: ");
                  String mText = scl.nextLine();
@@ -28,7 +30,12 @@ public class App {
                  Decipher theMessage = new Decipher (mText,mNumbers);
                  System.out.println("Your decrypted message is: " + theMessage.DecryptedText());
                  System.out.println("-----------------------------------------------");
-
-}
-}
-}
+              } else if (select.equals("#")) {
+                 programRunning = false;
+                 System.exit(0);
+              } else {
+                 System.out.println("Invalid input!!!!");
+              }
+              }
+          }
+        }
