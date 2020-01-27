a4c83a2af13b8516d92b536b2a71120aa8a18607
jonahmakori <jonahmakori0@gmail.com>
2020-01-27 14:46:45 +0300

    All tests passed

--- src/main/java/Decipher.java
+++ src/main/java/Decipher.java
@@ -1,3 +1,49 @@
 public class Decipher {
+    private String mText;
+    private int mNumbers;
+    public Decipher(String Text,int Numbers) {
+        this.mText= Text;
+        this.mNumbers=Numbers;
+    }
 
+    public String getText() {
+        return mText;
+    }
+
+    public int getNumbers() {
+        return mNumbers;
+    }
+    public String getDecryptedText(){
+        String decryptMessage = "";
+        for(int i=0; i < mText.length();i++)
+
+        {
+            char alphabet = mText.charAt(i);
+            if(alphabet >= 'a' && alphabet <= 'z')
+            {
+                alphabet = (char) (alphabet - mNumbers);
+
+                if(alphabet < 'a') {
+                    alphabet = (char) (alphabet-'a'+'z'+1);
+                }
+                decryptMessage = decryptMessage + alphabet;
+            }
+            else if(alphabet >= 'A' && alphabet <= 'Z')
+            {
+                alphabet = (char) (alphabet - mNumbers);
+
+                if (alphabet < 'A') {
+                    alphabet = (char) (alphabet-'A'+'Z'+1);
+                }
+                decryptMessage = decryptMessage + alphabet;
+            }
+            else
+            {
+                decryptMessage = decryptMessage + alphabet;
+            }
+        }
+        System.out.println(" decrypt message : " + decryptMessage);
+        return decryptMessage;
+    }
 }
+
--- src/test/java/DecipherTest.java
+++ src/test/java/DecipherTest.java
@@ -3,5 +3,28 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 public class DecipherTest {
+    @Test
+    public void  runCipher_gettingTheText_ArrayList() {
+        Cipher testCipher = new Cipher("bye", 2);
+        assertEquals("bye", testCipher.getText());
 
+    }
+    @Test
+    public void runCipher_gettingTheNumbers_ArrayList(){
+        Cipher testCipher = new Cipher("bye",2);
+        assertEquals(2,testCipher.getNumbers());
+    }
+
+
+    @Test
+    public void runCipher_gettingTheDecryptedText_ArrayList(){
+        Decipher testCipher = new Decipher("dag",2);
+        assertEquals("bye",testCipher.getDecryptedText());
+    }
 }
+
+
+
+
+
+
